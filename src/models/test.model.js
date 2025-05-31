afdc170e2197bb824d9d155e826f1b1d7c2f6c68
Shubh Verma <shubhverma2003@gmail.com>
2025-05-31 10:58:37 +0530

    refactor: update test schema to remove syllabus field

--- src/models/test.model.js
+++ src/models/test.model.js
@@ -1,36 +1,41 @@
 import mongoose, { Schema } from "mongoose";
 
-const testSchema = new Schema({
-  studentId: {
-    type: Schema.Types.ObjectId,
-    ref: "Student",
+const testSchema = new Schema(
+  {
+    studentId: {
+      type: Schema.Types.ObjectId,
+      ref: "Student",
+    },
+    chapterName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    topicName: {
+      type: String,
+      trim: true,
+    },
+    testScore: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
+    },
+    difficultyLevel: {
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      required: true,
+    },
+    subject: {
+      type: Schema.Types.ObjectId,
+      ref: "Subject",
+      required: true,
+    },
   },
-  chapterName: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  topicName: {
-    type: String,
-    trim: true,
-  },
-  testScore: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 10,
-  },
-  difficultyLevel: {
-    type: String,
-    enum: ["easy", "medium", "hard"],
-    required: true,
-  },
-  syllabus: {
-    type: Schema.Types.ObjectId,
-    ref: "Syllabus",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Test = mongoose.models.Test || mongoose.model("Test", testSchema);
 
