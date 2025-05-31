998a307aa8996c8c497a04159f4353c7bd7ceb5c
Shubh Verma <shubhverma2003@gmail.com>
2025-05-31 10:58:45 +0530

    feat: add subject model and remove syllabus model

---
+++ src/models/subject.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import Student from "./student.model";
+
+const chapterSchema = new mongoose.Schema({
+  chapterName: {
+    type: String,
+  },
+  topics: {
+    type: [String],
+  },
+});
+
+const subjectSchema = new mongoose.Schema(
+  {
+    subjectName: {
+      type: String,
+      required: true,
+    },
+    subjectDescription: {
+      type: String,
+    },
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+    chapters: [chapterSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Subject =
+  mongoose.models.Subject || mongoose.model("Subject", subjectSchema);
+
+export default Subject;
